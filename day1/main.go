a7a55ce68ca50183b81872ebb137be7aa9e898af
Connor Hall <consparkzz@gmail.com>
2025-12-05 23:41:27 +0000

    Some more resturcturing and actually almost working part 2?

--- day1/main.go
+++ day1/main.go
@@ -38,11 +38,12 @@ func main() {
 	startPosition := 50
 
 	realPass := 0
-	if *puzzlePart == 1 {
+	switch *puzzlePart {
+	case 1:
 		realPass = PuzzlePart1(startPosition, file)
-	} else if *puzzlePart == 2 {
+	case 2:
 		realPass = PuzzlePart2(startPosition, file)
-	} else {
+	default:
 		fmt.Println("Invalid part number! Please enter 1 or 2")
 	}
 
@@ -50,14 +51,13 @@ func main() {
 }
 
 func PuzzlePart1(startPosition int, file *os.File) int {
-	currentPosition := startPosition
 	realPass := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		fmt.Printf("Current password %d\n", realPass)
+		fmt.Printf("currentPosition %d\n", startPosition)
 		line := scanner.Text()
 		fmt.Println(line)
-		fmt.Printf("%c\n", line[0])
-		fmt.Printf("%v\n", line[1:])
 		direction := rune(line[0])
 		count, err := strconv.Atoi(line[1:])
 		if err != nil {
@@ -66,15 +66,80 @@ func PuzzlePart1(startPosition int, file *os.File) int {
 		}
 
 		if direction == 'R' {
-			currentPosition += count
+			startPosition += count
 		} else {
-			currentPosition -= count
+			startPosition -= count
 		}
 
-		currentPosition %= 100
-		if currentPosition == 0 {
+		startPosition = ClampToRange(0, 100, startPosition%100)
+		if startPosition == 0 {
 			realPass++
 		}
+		fmt.Println()
+	}
+
+	if scanErr := scanner.Err(); scanErr != nil {
+		fmt.Printf("Error while scanning file! %v\n", scanErr)
+		return -1
+	}
+	return realPass
+}
+
+// Apparently there isn't a native way for this in go? I'm surprised
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+// Pass end position BEFORE modulus
+func PassedZeroCount(startPosition int, endPosition int) int {
+	diff := absDiffInt(endPosition, startPosition)
+	if endPosition >= 100 || endPosition <= 0 {
+		return (diff / 100) + 1
+	}
+	return 0
+}
+
+func ClampToRange(startRange int, endRange int, value int) int {
+	if value < startRange {
+		diff := startRange - value
+		return endRange - diff
+	} else if value >= endRange {
+		diff := value - endRange
+		return startRange + diff
+	}
+	return value
+}
+
+func PuzzlePart2(startPosition int, file *os.File) int {
+	realPass := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Printf("Current password %d\n", realPass)
+		line := scanner.Text()
+		fmt.Printf("currentPosition %d\n", startPosition)
+		fmt.Println(line)
+		direction := rune(line[0])
+		count, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Printf("Error while parsing line! %v\n", err)
+			continue
+		}
+
+		newPosition := startPosition
+
+		if direction == 'R' {
+			newPosition += count
+		} else {
+			newPosition -= count
+		}
+
+		realPass += PassedZeroCount(startPosition, newPosition)
+
+		startPosition = ClampToRange(0, 100, newPosition%100)
+		fmt.Println()
 	}
 
 	if scanErr := scanner.Err(); scanErr != nil {
