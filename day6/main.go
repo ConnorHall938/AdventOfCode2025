b8720597c296dbf360bc592446311a762f1ace62
Connor Hall <consparkzz@gmail.com>
2025-12-07 01:33:37 +0000

    Okay not even the parsing will work for part 2

--- day6/main.go
+++ day6/main.go
@@ -39,9 +39,9 @@ func main() {
 	numberFresh := 0
 	switch *puzzlePart {
 	case 1:
-		numberFresh = Puzzle(file, false)
-	case 2:
 		numberFresh = Puzzle(file, true)
+	case 2:
+		numberFresh = Puzzle(file, false)
 	default:
 		fmt.Println("Invalid part number! Please enter 1")
 	}
@@ -51,9 +51,8 @@ func main() {
 
 // Ahhh why does no language natively support this?!
 func IntegerPow(num, exponent int) int {
-	//IDC about 0  because we will not use that here.
-	curVal := num
-	for i := range exponent - 1 {
+	curVal := 1
+	for i := range exponent {
 		curVal *= num
 		i = i
 	}
@@ -72,6 +71,48 @@ func IntMin(x, y int) int {
 		return y
 	}
 	return x
+}
+
+func DoItWrong(file *os.File) int {
+
+	return 0
+}
+
+func DoItNormally(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	lineCounter := 0
+	line := "" // Litterally only outside the loop so I can break and still use the line
+	numbers := [][]string{}
+	// Parse the lines
+	for scanner.Scan() {
+		fmt.Printf("Scanning line %d of input\n", lineCounter)
+		line = scanner.Text()
+		if line[0] == '*' || line[0] == '+' {
+			break
+		}
+		numberStrings := strings.Split(line, " ")
+		filteredNumberStrings := []string{}
+		for _, val := range numberStrings {
+			if val != "" {
+				filteredNumberStrings = append(filteredNumberStrings, val)
+			}
+		}
+
+		numbers = append(numbers, make([]string, len(filteredNumberStrings)))
+		copy(numbers[lineCounter], filteredNumberStrings)
+		lineCounter++
+	}
+
+	// Parse the operation and calculate
+	operationStrings := strings.Split(line, " ")
+	filteredOperationStrings := []string{}
+	for _, val := range operationStrings {
+		if val != "" {
+			filteredOperationStrings = append(filteredOperationStrings, val)
+		}
+	}
+
+	return withHumanMath(numbers, filteredOperationStrings)
 }
 
 func withHumanMath(inputs [][]string, operands []string) int {
@@ -106,44 +147,10 @@ func withHumanMath(inputs [][]string, operands []string) int {
 }
 
 func Puzzle(file *os.File, fixTheirStupidAssMath bool) int {
-	scanner := bufio.NewScanner(file)
-	runningTotal := 0
-	lineCounter := 0
-	line := "" // Litterally only outside the loop so I can break and still use the line
-	numbers := [][]string{}
-	// Parse the lines
-	for scanner.Scan() {
-		fmt.Printf("Scanning line %d of input\n", lineCounter)
-		line = scanner.Text()
-		if line[0] == '*' || line[0] == '+' {
-			break
-		}
-		numberStrings := strings.Split(line, " ")
-		filteredNumberStrings := []string{}
-		for _, val := range numberStrings {
-			if val != "" {
-				filteredNumberStrings = append(filteredNumberStrings, val)
-			}
-		}
-
-		numbers = append(numbers, make([]string, len(filteredNumberStrings)))
-		copy(numbers[lineCounter], filteredNumberStrings)
-		lineCounter++
-	}
-
-	// Parse the operation and calculate
-	operationStrings := strings.Split(line, " ")
-	filteredOperationStrings := []string{}
-	for _, val := range operationStrings {
-		if val != "" {
-			filteredOperationStrings = append(filteredOperationStrings, val)
-		}
-	}
 
 	if fixTheirStupidAssMath {
-		return 0
+		return DoItNormally(file)
 	}
-	return withHumanMath(numbers, filteredOperationStrings)
+	return DoItWrong(file)
 
-	return runningTotal
 }
