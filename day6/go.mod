a41115bb35e6e1ac3c179c87b83ef9433fabfc63
Connor Hall <consparkzz@gmail.com>
2025-12-07 00:15:01 +0000

    Day6 part 1

---
+++ day6/go.mod
@@ -0,0 +1,3 @@
+module ConnorHall938/AdventOfCode2025/day6
+
+go 1.25.4
---
+++ day6/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const puzzleFileDefault = "puzzle.txt"
+
+func main() {
+
+	puzzlePart := flag.Int("Part", 1, "Which part of the puzzle? 1/2")
+	puzzleFile := flag.String("Input", puzzleFileDefault, "Name of the puzzle file")
+
+	flag.Parse()
+
+	if puzzleFile == nil {
+		fmt.Printf("Puzzle file cannot be empty!")
+		return
+	}
+
+	if puzzlePart == nil {
+		fmt.Printf("Puzzle part cannot be empty!")
+		return
+	}
+
+	file, err := os.Open(*puzzleFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		fmt.Printf("Error opening puzzle file! Exiting!")
+		return
+	}
+	defer file.Close()
+
+	numberFresh := 0
+	switch *puzzlePart {
+	case 1:
+		numberFresh = Puzzle(file)
+	case 2:
+		numberFresh = Puzzle(file)
+	default:
+		fmt.Println("Invalid part number! Please enter 1")
+	}
+
+	fmt.Printf("Homework answer is %d\n", numberFresh)
+}
+
+// Ahhh why does no language natively support this?!
+func IntegerPow(num, exponent int) int {
+	//IDC about 0  because we will not use that here.
+	curVal := num
+	for i := range exponent - 1 {
+		curVal *= num
+		i = i
+	}
+	return curVal
+}
+
+func IntMax(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func IntMin(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func Puzzle(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	runningTotal := 0
+	lineCounter := 0
+	line := "" // Litterally only outside the loop so I can break and still use the line
+	numbers := [][]int{}
+	// Parse the lines
+	for scanner.Scan() {
+		fmt.Printf("Scanning line %d of input\n", lineCounter)
+		line = scanner.Text()
+		if line[0] == '*' || line[0] == '+' {
+			break
+		}
+		numberStrings := strings.Split(line, " ")
+		filteredNumberStrings := []string{}
+		for _, val := range numberStrings {
+			if val != "" {
+				filteredNumberStrings = append(filteredNumberStrings, val)
+			}
+		}
+
+		numbers = append(numbers, make([]int, len(filteredNumberStrings)))
+
+		for i, val := range filteredNumberStrings {
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				fmt.Printf("Error converting string %v to int: %v\n", line, err)
+				return -1 // Handle the error appropriately
+			}
+			numbers[lineCounter][i] = num
+		}
+		lineCounter++
+	}
+
+	// Parse the operation and calculate
+	operationStrings := strings.Split(line, " ")
+	filteredOperationStrings := []string{}
+	for _, val := range operationStrings {
+		if val != "" {
+			filteredOperationStrings = append(filteredOperationStrings, val)
+		}
+	}
+
+	for idx, val := range filteredOperationStrings {
+		currentTotal := 0
+		switch val {
+		case "*":
+			currentTotal = 1
+			for _, numbersList := range numbers {
+				currentTotal *= numbersList[idx]
+			}
+		case "+":
+			currentTotal = 0
+			for _, numbersList := range numbers {
+				currentTotal += numbersList[idx]
+			}
+		}
+		runningTotal += currentTotal
+	}
+
+	return runningTotal
+}
